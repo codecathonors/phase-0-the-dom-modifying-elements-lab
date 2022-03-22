05af3d0be5a5bab511a54c43e8511b83a3136fe5
CatherineHonors <cathonors@gmail.com>
2022-03-22 14:58:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//someElement.removeChild(someChildElement);
+const mainElement = document.getElementById('main');
+
+mainElement.remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Cat is the champion";
